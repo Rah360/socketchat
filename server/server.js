7295ee9bbde5ae156bd961f73407a11703a2b750
rahul home <rahul@tapcubestudios.com>
2018-12-01 20:53:00 +0530

    broadcast

--- server/server.js
+++ server/server.js
@@ -4,24 +4,37 @@ const http=require('http')
 const socketio=require('socket.io')
 
 let app=express()
-app.use(express.static(path.join(__dirname,"../public")))
+app.use(express.static(path.join(__dirname,"../public/")))
 let server=http.Server(app)
 let io=socketio(server)
+
+
+let user=0
+let totalmsg=0
+
 io.on("connection",(socket)=>{
-    console.log("new connection");
-    socket.on("disconnect",()=>{
-        console.log("closed")
+    console.log("user connected,total users:=",(user+1))
+    socket.on("createEmail",(data)=>{
+        totalmsg=+1
+        console.log("new msg ",data)
+        io.emit('newEmail',{
+            from:data.from,
+            to:data.to
+        })
     })
     socket.emit("newEmail",{
-        from:"me",
-        to:"rahul"
+        from:"rahul",
+        to:"client"
     })
-    socket.on("createEmail",(data)=>{
-        console.log(data)
+    socket.on("disconnect",()=>{
+        user=-1
+        console.log("user disconnected,total users",user)
     })
+    
+
 })
 
 let port=process.env.PORT || 3000
 server.listen(port,()=>{
-    console.log("connected to server")
+    console.log("connected to server 3000");
 })
